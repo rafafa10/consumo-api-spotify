deae83fc3bd2acfe3dad8428794bc6c0c957cd86
Rafael Souza <1203rafafa@gmail.com>
2022-03-20 22:27:17 -0300

    primeiro commit

---
+++ src/main/java/br/com/estudo/consumoapispotify/ConsumoApiSpotifyApplication.java
@@ -0,0 +1,13 @@
+package br.com.estudo.consumoapispotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumoApiSpotifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumoApiSpotifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/estudo/consumoapispotify/ConsumoApiSpotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.estudo.consumoapispotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumoApiSpotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
